2ae1a04ca367c8973be6bfb33c2dc90832b4942b
abakre5 <abakre5@gmail.com>
2018-04-16 21:06:22 +0530

    First 4

---
+++ Assignment4/Release/src/Assignment4.d
@@ -0,0 +1,6 @@
+src/Assignment4.d: ../src/Assignment4.c /usr/lib/openmpi/include/mpi.h \
+ /usr/lib/openmpi/include/mpi_portable_platform.h
+
+/usr/lib/openmpi/include/mpi.h:
+
+/usr/lib/openmpi/include/mpi_portable_platform.h:
